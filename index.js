3f57e9753323cc1b66ad97eb8e2721cc13fa4ef3
bhoopendra3shukla <bhoopendrashukla3@gmail.com>
2023-12-12 17:26:35 +0530

    test file

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ fs.writeFile("/text.txt","hello Bhoopendra async",(err)=>{
     }
 });
 
-//read the file
+//read the file Sync
 const result=fs.readFileSync("./contect.txt","utf-8");
+console.log(result)
+
+//read the file Async
+fs.readFile("./contect.txt","utf-8",(err,result1)=>{
+    if (err) throw err;
+    else console.log(result1) 
+});
 console.log(result)
