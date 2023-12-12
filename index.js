93d5751af5df588153e5eef2df4d70073cef7da6
bhoopendra3shukla <bhoopendrashukla3@gmail.com>
2023-12-12 17:13:36 +0530

    fs module in node js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const add=require("./math")
-console.log("this is test file");
-add();
+const fs=require("fs");//build in module in node js
+
+//Sync...
+fs.writeFileSync("./text.txt","hello Bhoopendra");
+
+//Async...
+fs.writeFile("/text.txt","hello Bhoopendra async",(err)=>{
+    if(err){
+        console.log(err)
+    }
+});
